217f4dee3bf9d80983f571912cfc3404c340081e
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-24 15:37:05 +0200

    creazione pagina memes due div

---
+++ resources/views/memes.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleMemes.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div>
+        <img src="https://i.pinimg.com/originals/85/9a/4e/859a4e79f2fc763ba9d51e36eb9484d2.jpg" alt="">
+        <img src="http://images3.memedroid.com/images/UPLOADED576/60d1eefd7bf4b.jpeg" alt="">
+        <img src="https://titterfun.com/api/assets/image/5y2xxltwlq0b.jpg" alt="">
+    </div>
+    <div>
+        <img src="https://i.chzbgr.com/full/9239495168/hADF48F79/lotr-meme-about-talking-to-your-dog-even-though-it-cant-answer-back" alt="">
+        <img src="https://piximus.net/media2/61194/lotr-memes-1.jpg" alt="">
+        <img src="https://pbs.twimg.com/media/EbFi-C2U8AA2G0M.jpg" alt="">
+    </div>
+
+        
+</body>
+</html>
