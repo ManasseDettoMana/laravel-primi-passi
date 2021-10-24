c1e9d561df9e2c7e9c64c3de6eb7638a290be951
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-10-24 15:12:12 +0200

    first commit

---
+++ resources/views/egg.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>egg</title>
+</head>
+<body>
+    <h1>This is an Easter Egg :)</h1>
+    <img src="https://timgate.it/ecm/online-content/7a6/92d/381/chi-e-luis-sal-eta-biografia-e-vero-nome_2760e445.jpg" alt="">
+
+    <p>no, this is not me.. it's my double..</p>
+</body>
+</html>
